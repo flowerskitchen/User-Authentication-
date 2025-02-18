04ebdc02136d16203960e209c67c0546c3d66033
flowerskitchen <flowerskitchen4@gmail.com>
2025-02-18 19:22:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 
+
+
 const app = express();
 const port = 3000;
 const SECRET_KEY = process.env.SECRET_KEY || 'your_secret_key';
